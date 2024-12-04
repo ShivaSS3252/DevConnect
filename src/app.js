d1cfdb6b097b29e1fffad637bd2f02a944fc4aa6
= <=>
2024-12-04 19:31:39 +0530

    app js route handlers

--- src/app.js
+++ src/app.js
@@ -6,9 +6,27 @@ app.get("/user/:userid", (req, res) => {
   console.log(req.params);
   res.send({ firstName: "Shiva", lastName: "Sharanya" });
 });
-
+app.use(
+  "/userdata",
+  (req, res, next) => {
+    res.send("first routehandler");
+    next();
+    console.log("first routehandler");
+  },
+  (req, res, next) => {
+    console.log("second routehandler");
+    next();
+    // res.send("second routehandler");
+  },
+  (req, res, next) => {
+    console.log("third routehandler");
+    next();
+    // res.send("third routehandler");
+  }
+);
 //this will match all the http method API calls to /test
 app.use("/test", (req, res) => {
+  //This callback function is known as Route handler
   res.send("Hello from the test");
 });
 
