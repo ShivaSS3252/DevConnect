2d10bc4d8499a69ff2d6f41c90f5cbfbe441d33a
= <=>
2024-12-04 17:14:45 +0530

    pp js changes params

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
-app.use("/test", (req, res) => {
-  res.send("Hello from the server");
+//this will only handle GET call to /user
+
+app.get("/user/:userid", (req, res) => {
+  console.log(req.params);
+  res.send({ firstName: "Shiva", lastName: "Sharanya" });
 });
+
+//this will match all the http method API calls to /test
+app.use("/test", (req, res) => {
+  res.send("Hello from the test");
+});
+
 app.listen(3000, () => {
   console.log("server is succefully listening");
 });
