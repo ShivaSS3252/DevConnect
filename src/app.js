5ebd5272c4b6dc47a87aba173c0a8c6e8de51bbd
= <=>
2024-12-16 22:03:22 +0530

    schema validations

--- src/app.js
+++ src/app.js
@@ -2,20 +2,57 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
+app.use(express.json());
 app.post("/signup", async (req, res) => {
   const userObj = {
-    firstName: "Sharanya",
-    lastName: "shiva",
-    emailId: "shiva@gmail.com",
-    password: "shiva@123",
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    emailId: req.body.emailId,
+    password: req.body.password,
   };
   //Creating a new instance of user model
-  const user = new User(userObj);
+  const user = new User(req.body);
   try {
     await user.save();
     res.send("User Added Successfully");
   } catch (err) {
-    res.status(400).send("errro saving the user" + err.message);
+    res.status(400).send("error saving the user" + err.message);
+  }
+});
+
+//Feed API-get all the users from database
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("error saving the user" + err.message);
+  }
+});
+//Delete the user
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  console.log("userId", req.body.userId);
+  try {
+    const user = await User.findByIdAndDelete(userId);
+
+    res.send("User deleted successfully");
+  } catch (err) {
+    res.status(400).send("error saving the user" + err.message);
+  }
+});
+
+//update the user
+app.patch("/user", async (req, res) => {
+  const data = req.body;
+  const userId = req.body.userId;
+  try {
+    await User.findByIdAndUpdate({ _id: userId }, data, {
+      runValidators: true,
+    });
+    res.send("User updated successfully");
+  } catch (err) {
+    res.status(400).send("update failed" + err.message);
   }
 });
 connectDB()
