43e45c58060015f23f99b92b06322c2630e9f95d
= <=>
2025-01-05 22:53:03 +0530

    ueries and indexes in Db and handle corner cases

--- src/routes/request.js
+++ src/routes/request.js
@@ -1,9 +1,54 @@
 const express = require("express");
 const requestRouter = express.Router();
 const { userauth } = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
+requestRouter.post(
+  "/request/send/:status/:toUserId",
+  userauth,
+  async (req, res) => {
+    try {
+      const fromUserId = req.user._id;
+      const toUserId = req.params.toUserId;
+      const status = req.params.status;
+      const allowedStatus = ["ignored", "interested"];
+      if (!allowedStatus.includes(status)) {
+        return res
+          .status(400)
+          .json({ message: "Invalid status type: " + status });
+      }
 
-requestRouter.post("/sendConnectionRequest", userauth, async (req, res) => {
-  const user = req.user;
-  res.send(user.firstName + " sent connection request");
-});
+      //check if there is existing connectionRequest
+      const existingConnectionRequest = await ConnectionRequest.findOne({
+        $or: [
+          { fromUserId, toUserId },
+          { fromUserId: toUserId, toUserId: fromUserId },
+        ],
+      });
+      if (existingConnectionRequest) {
+        return res.status(400).send({
+          message: "Connection Request Already exist",
+        });
+      }
+
+      const toUser = await User.findById(toUserId);
+      if (!toUser) {
+        return res.status(404).json({ message: "User not found" });
+      }
+      const connectionRequest = new ConnectionRequest({
+        fromUserId,
+        toUserId,
+        status,
+      });
+      const data = await connectionRequest.save();
+      res.json({
+        message:
+          req.user.firstName + " is " + status + " in " + toUser.firstName,
+        data,
+      });
+    } catch (err) {
+      res.status(400).send("Error: " + err.message);
+    }
+  }
+);
 module.exports = requestRouter;
